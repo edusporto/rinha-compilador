5782040a1ea094dae68025f852c513bec41f4225
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2023-09-21 21:16:44 +0200

    Fix test names to better represent what they do

--- test/Test/Syntax.hs
+++ test/Test/Syntax.hs
@@ -11,13 +11,13 @@ import Test.Tasty.HUnit
 
 syntaxTests :: [TestTree]
 syntaxTests =
-  [ testCase "Parameter parsing" $
+  [ testCase "Parameter encoding" $
       encode (Parameter "Hi") @?= [i|{"text":"Hi"}|],
-    testCase "Int parsing" $
+    testCase "Int encoding" $
       encode (Int 5) @?= [i|{"kind":"Int","value":5}|],
     testCase "Int decoding" $
       decode [i|{"kind":"Int","value":4,"bla":0}|] @?= Just (Int {valueInt = 4}),
-    testCase "Operator parsing" $
+    testCase "Operator decoding" $
       eitherDecode [i|"Gt"|] @?= Right Gt,
     programParsingTest "Fibonacci" "rinha-de-compiler/files/fib.json",
     programParsingTest "Combination" "rinha-de-compiler/files/combination.json",
